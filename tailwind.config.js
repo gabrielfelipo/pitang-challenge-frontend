2538417f3fbc8c9f5511e2da54f962acf1ba6114
Felipo <gabriel.felipo2010@gmail.com>
2024-07-16 19:42:31 -0300

    feature(base-project): Add form dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { nextui } = require('@nextui-org/react')
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./index.html",
@@ -7,5 +10,68 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui({
+    defaultTheme: 'light',
+    defaultExtendTheme: 'light',
+    layout: {
+      fontSize: {
+        tiny: '0.875rem',
+        small: '1rem',
+        medium: '1.125rem',
+        large: '1.25rem',
+        DEFAULT: '1.125rem',
+      },
+    },
+    themes: {
+      light: {
+        layout: {}, // light theme layout tokens
+        colors: {
+          background: '#120118',
+          foreground: '#FFFFFF',
+          default: {
+            foreground: '#FFFFFF',
+            400: '#FFFFFF',
+            600: '#FFFFFF',
+            DEFAULT: '#543861',
+          },
+          primary: {
+            100: '#EAD2F6',
+            200: '#774D8B',
+            300: '#543861',
+            400: '#451953',
+            500: '#300E3B',
+            600: '#331F3D',
+            700: '#1D0227',
+            800: '#120118',
+            900: '#120118',
+            foreground: '#FFFFFF',
+            DEFAULT: '#300E3B',
+          },
+          secondary: {
+            /*  100: '#FDECDA',
+            200: '#FCD4B7',
+           */ 300: '#F8876C' /* 
+            400: '#EE9474', */,
+            500: '#E36648',
+            /*    600: '#C34534',
+            700: '#A32924',
+            800: '#83161A',
+            900: '#6C0D17', */
+            foreground: '#FFFFFF',
+            DEFAULT: '#E36648',
+          },
+          focus: '#FFFFFF',
+          danger: '#F31260',
+          success: '#17C964',
+          dark: '#18181B',
+          tooltip: '#0070F0',
+        },
+      },
+      dark: {
+        layout: {}, // dark theme layout tokens
+        colors: {}, // dark theme colors
+      },
+    },
+  }),],
 }
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import tspaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tspaths()],
+  build: {
+    minify: 'esbuild',
+  },
+  worker: {
+    plugins: () => [react()],
+    format: 'es',
+  },
 })
