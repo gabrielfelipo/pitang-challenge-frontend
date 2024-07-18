40689e0e140f1f2b2bee780d4aaa33027fcf29d4
Felipo <gabriel.felipo2010@gmail.com>
2024-07-18 10:23:11 -0300

    feature(form-schedule): create forms components

--- src/components/forms/Input.tsx
+++ src/components/forms/Input.tsx
@@ -3,36 +3,29 @@ import { Controller, useFormContext } from 'react-hook-form'
 import { InputProps } from './types'
 
 export const Input = ({ ...props }: InputProps) => {
-    const form = useFormContext()
+  const form = useFormContext()
 
-    return (
-        <Controller 
-            control={form.control}
-            render={({
-                field, 
-                fieldState
-            }) => (
-                <NUIInput 
-                required={!props.isOptional}
-                isInvalid={!!fieldState.error}
-                errorMessage={fieldState.error?.message}
-                maxLength={10}
-                labelPlacement='inside'
-                variant='bordered'
-                classNames={{
-                    errorMessage: 'text-danger',
-                    inputWrapper: 'border-2 transition-colors',
-                    label: 'font-semibold',
-                    input: ['placeholder:text-default-600/50'],
-                  }}
-                {...props}
-                {...field}
-                value={field.value}
-                onChange={field.onChange}
-                />
-            )}
-            name={props.name}
+  return (
+    <Controller
+      control={form.control}
+      render={({ field, fieldState }) => (
+        <NUIInput
+          required={!props.isOptional}
+          isInvalid={!!fieldState.error}
+          errorMessage={fieldState.error?.message}
+          maxLength={10}
+          labelPlacement="inside"
+          color="default"
+          variant="bordered"
+          className="text-black"
+          classNames={{ inputWrapper: 'border-grey-300' }}
+          {...props}
+          {...field}
+          value={field.value}
+          onChange={field.onChange}
         />
-
-    )
+      )}
+      name={props.name}
+    />
+  )
 }
--- src/components/forms/index.tsx
+++ src/components/forms/index.tsx
@@ -1,19 +1,21 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { memo } from 'react'
 import {
-    FormProvider,
-    SubmitHandler,
-    UseFormProps,
-    UseFormReturn,
-    useForm,
-    useFormContext,
-  } from 'react-hook-form'
+  FormProvider,
+  SubmitHandler,
+  UseFormProps,
+  UseFormReturn,
+  useForm,
+  useFormContext,
+} from 'react-hook-form'
 import { Except } from 'type-fest'
 import { z } from 'zod'
 
 import { Input } from './Input'
+import { DateInput } from './DateInput'
+import { Select } from './Select'
 
-const components = {Input}
+const components = { Input, DateInput, Select }
 
 interface UseZodFormProps<S extends z.ZodSchema>
   extends Exclude<UseFormProps<z.infer<S>>, 'resolver'> {
@@ -25,67 +27,70 @@ type FormComponent<
   T extends (typeof components)[keyof typeof components],
 > = (props: Parameters<T>[0] & { name: keyof z.infer<S> }) => ReturnType<T>
 
-interface UseZodFormReturn<S extends z.ZodSchema> 
-    extends UseFormReturn<z.infer<S>> {}
+interface UseZodFormReturn<S extends z.ZodSchema>
+  extends UseFormReturn<z.infer<S>> {}
 
-export const useZodFormContext = < S extends z.ZodSchema >(): 
-    UseZodFormReturn<S> => useFormContext<z.infer<S>>()
+export const useZodFormContext = <
+  S extends z.ZodSchema,
+>(): UseZodFormReturn<S> => useFormContext<z.infer<S>>()
 
 export const useZodForm = <S extends z.ZodSchema>({
-    schema,
-    ...formProps
-  }: UseZodFormProps<S>): UseZodFormReturn<S> =>
-    useForm({
-      ...formProps,
-      mode: 'onTouched',
-      resolver: zodResolver(schema),
-    })
+  schema,
+  ...formProps
+}: UseZodFormProps<S>): UseZodFormReturn<S> =>
+  useForm({
+    ...formProps,
+    mode: 'onTouched',
+    resolver: zodResolver(schema),
+  })
 
 type ZodFormProps<S extends z.ZodSchema> = {
-    onSubmit: SubmitHandler<z.infer<S>>
-    children: (props: {
-      form: UseFormReturn<z.TypeOf<S>, unknown, undefined>
-      Input: FormComponent<S, typeof Input>
-    }) => JSX.Element
-    className?: string
-    action?: string
-    method?: string
-  } & Except<UseFormProps<z.infer<S>>, 'resolver'> &
-    (
-      | {
-          schema: S
-        }
-      | { form: UseFormReturn<z.infer<S>> }
-    )
+  onSubmit: SubmitHandler<z.infer<S>>
+  children: (props: {
+    form: UseFormReturn<z.TypeOf<S>, unknown, undefined>
+    Input: FormComponent<S, typeof Input>
+    DateInput: FormComponent<S, typeof DateInput>
+    Select: FormComponent<S, typeof Select>
+  }) => JSX.Element
+  className?: string
+  action?: string
+  method?: string
+} & Except<UseFormProps<z.infer<S>>, 'resolver'> &
+  (
+    | {
+        schema: S
+      }
+    | { form: UseFormReturn<z.infer<S>> }
+  )
 
 export const ZodForm = memo(
-    <S extends z.ZodSchema>({
-      children: Children,
-      onSubmit,
-      className,
-      defaultValues,
-      ...props
-    }: ZodFormProps<S>) => {
-      const form =
-        'schema' in props
-          ? useForm({
-              mode: 'onTouched',
-              ...props,
-              resolver: zodResolver(props.schema),
-              defaultValues,
-            })
-          : props.form
-   
-      return (
-        <FormProvider {...form}>
-          <form
-            className={className}
-            onSubmit={form.handleSubmit(onSubmit)}
-            {...props}
-          >
-            {Children({ ...components, form })}
-          </form>
-        </FormProvider>
-      )
-    },
-  )
+  <S extends z.ZodSchema>({
+    children: Children,
+    onSubmit,
+    className,
+    defaultValues,
+    ...props
+  }: ZodFormProps<S>) => {
+    const form =
+      'schema' in props
+        ? useForm({
+            mode: 'onTouched',
+            ...props,
+            resolver: zodResolver(props.schema),
+            defaultValues,
+          })
+        : props.form
+
+    return (
+      <FormProvider {...form}>
+        <form
+          className={className}
+          onSubmit={form.handleSubmit(onSubmit)}
+          {...props}
+        >
+          {Children({ ...components, form })}
+        </form>
+      </FormProvider>
+    )
+  }
+)
